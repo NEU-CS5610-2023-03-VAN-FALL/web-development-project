c7809b26662ed77500a9a621e212a166183545ec
zhiminV <122182731+zhiminV@users.noreply.github.com>
2023-12-04 22:35:06 -0800

    add external api to change currency

--- client/src/components/Order.jsx
+++ client/src/components/Order.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios'; 
 import "../style/orderNow.css";
 
 export default function Order() {
@@ -9,6 +10,11 @@ export default function Order() {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
   const [drinks, setDrinks] = useState([]);
   const [cart, setCart] = useState([]);
+  const [selectedCurrency, setSelectedCurrency] = useState("CAD");
+
+  const calculateTotalAmount = () => {
+    return cart.reduce((total, drink) => total + drink.price, 0);
+  };
 
   const addToCart = (productId) => {
     const selectedDrink = drinks.find((drink) => drink.productId === productId);
@@ -19,11 +25,6 @@ export default function Order() {
     const updatedCart = cart.filter((drink) => drink.productId !== productId);
     setCart(updatedCart);
   };
-
-  const calculateTotalAmount = () => {
-    return cart.reduce((total, drink) => total + drink.price, 0);
-  };
-
   const handleCheckOut = async () => {
     try {
       const accessToken = await getAccessTokenSilently();
@@ -46,13 +47,40 @@ export default function Order() {
       });
 
       if (response.ok) {
-        
         setCart([]);
       } else {
         console.error("Error during checkout. Server response:", response.statusText);
       }
     } catch (error) {
       console.error("Error during checkout", error);
+    }
+  };
+
+  const handleCurrencyChange = async (newCurrency) => {
+    try {
+      const exchangeRateResponse = await axios.get('https://currency-exchange.p.rapidapi.com/exchange', {
+        params: {
+          from: selectedCurrency,
+          to: newCurrency,
+        },
+        headers: {
+          'X-RapidAPI-Key': '9f0ef8a211msh91385553ca25698p125414jsna925a7b30b1a',
+          'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com',
+        },
+      });
+
+      const exchangeRate = exchangeRateResponse.data;
+
+      const convertedCart = cart.map((item) => {
+        const convertedPrice = item.price * exchangeRate;
+        const roundedPrice = newCurrency === 'CAD' ? Math.round(convertedPrice) : convertedPrice;
+        return { ...item, price: roundedPrice };
+      });
+
+      setCart(convertedCart);
+      setSelectedCurrency(newCurrency);
+    } catch (error) {
+      console.error("Error changing currency:", error);
     }
   };
 
@@ -95,9 +123,21 @@ export default function Order() {
 
       <div>
         <h2>Shopping Cart</h2>
+        <div>
+          <label htmlFor="currency">Select Currency:</label>
+          <select
+            id="currency"
+            value={selectedCurrency}
+            onChange={(e) => handleCurrencyChange(e.target.value)}
+          >
+            <option value="USD">USD</option>
+            <option value="CAD">CAD</option>
+            
+          </select>
+        </div>
         <ul>
           {cart.map((item) => (
-            <li key={item.productId}>{item.productName} </li>
+            <li key={item.productId}>{item.productName} - ${item.price}</li>
           ))}
         </ul>
         <p>Total Amount: ${calculateTotalAmount()}</p>
